80b514f7088cbc98cc76a7883964632565b73748
mje1281 <m.ernst@rushordertees.com>
2017-03-06 16:26:09 -0500

    condense space time outer calls to single call and prevent triggering when at the end of the loop

--- morse-code/index.js
+++ morse-code/index.js
@@ -4,27 +4,31 @@ function transmitter(options, callback){
   var message = options.message;
   var toggle = options.toggle;
   var timeouter = options.timeouter;
+  var messageParts = message.split(' ');
   
-  var symbols = [];
-  for (var i = 0; i < message.length; i++){
-    var code = (message[i] == ' ') ? 'space' : codes['message'[i]];
-    if(code !== 'space'){
-      for(var j = 0; j < code.length; j++){
-        if(code[j] === '.'){
+  for(var a = 0; a < messageParts.length; a++){
+    for (var i = 0; i < messageParts[a].length; i++){
+      var code = codes['messageParts[a]'[i]];
+        for(var j = 0; j < code.length; j++){
           toggle();
-          timeouter(toggle(), 1)
-          timeouter(toggle(), 1)
-        } else if(code[j] === '-'){
-          toggle();
-          timeouter(toggle(), 3);
-          timeouter(toggle(), 1);
-          clearTimeout(timeouter);
+          if(code[j] === '.'){
+            timeouter(toggle(), 1)          
+          } else if(code[j] === '-'){
+            timeouter(toggle(), 3);
+          }
+          if(j !== code.length -1){
+            timeouter(toggle(), 1)
+          }
         }
+      if(i !== messageParts[a].length - 1){
+        timeouter(toggle(), 3)  
       }
-    } else {
-      options.timeouter(options.toggle(), 7)
+    }
+    if(a !== messageParts.length -1) {
+      timeouter(toggle(), 7)
     }
   }
   callback();
+  
 }
 module.exports.transmitter = transmitter;
