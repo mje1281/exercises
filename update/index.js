a5e3006945833cce7af80050c8ecfe5dcf19f5e4
mje1281 <m.ernst@rushordertees.com>
2017-03-06 19:26:46 -0500

    move switch to a function to execute on the required command and add function to get nested property objects

--- update/index.js
+++ update/index.js
@@ -1,46 +1,62 @@
 function update(state, commands){  
   
   var newState = state;
-  var originalKeys = Object.keys(commands);
-  
-  var baseKey = originalKeys[originalKeys.length - 1];
-  var functionName =  baseKey.slice(1, baseKey.length)
-  
-  switch(functionName){
-    case('push'):
-      for(var i = 0; i < commands[baseKey].length; i++){
-        newState.push(commands[baseKey][i]);
-      }
-      break;
-    case('unshift'):
-      for(var i = 0; i < commands[baseKey].length; i++){
-        newState.unshift(commands[baseKey][i]);
-      }
-      break;
-    case('splice'):
-      for(var i = 0; i < commands[baseKey].length; i++){
-        var toRemove = commands[baseKey][i];    
-        newState.splice(parseInt(toRemove[0]), parseInt(toRemove[1]), parseInt(toRemove[2]));
-      }
-      break;
-    case('merge'):
-      for(var x in commands[baseKey]){
-        newState[x] = commands[baseKey][x];
-      }
-      break; 
-    case('set'):
-      newState = {};
-      for(var x in commands[baseKey]){
-        newState[x] = commands[baseKey][x];
-      }
-      break;  
-    case('apply'):
-        newState = commands[baseKey](state)
-      break; 
-    default:
-      break;
-     
+  var objectKeys = [];
+  getSubmissionKeys(commands);
+  function getSubmissionKeys(submission){
+    var originalKeys = Object.keys(submission)
+    objectKeys.push(originalKeys[0])
+    if(!originalKeys[0].includes('$')){
+      getSubmissionKeys(submission[originalKeys[0]]);
+    }
   }
+  
+  var hasOwnProperty  = Object.prototype.hasOwnProperty;
+  var directive = objectKeys[objectKeys.length - 1];
+  checkObject(commands, objectKeys, newState, 0);
+  
+  function checkObject(submission, keys, newObject, interval){  
+    if(!Array.isArray(submission[directive])){
+      if(hasOwnProperty.call(submission, directive)){
+        change = submission[directive]
+        applyDirective(directive, submission, change, newObject)
+      } else {
+          checkObject(submission[keys[interval++]], keys, newObject[keys[interval++]], interval++);    
+      }
+      
+    } else {
+      var change = commands[directive][0]
+      applyDirective(directive, submission, change, newState)
+    }
+  }
+  
+  function applyDirective(directive, commands, change, object){
+    switch(directive){
+      case('$push'):
+          object.push(change)
+        break;
+      case('$unshift'):
+          object.unshift(change);
+        break;
+      case('$splice'):   
+          object.splice(parseInt(change[0]), parseInt(change[1]), parseInt(change[2]));
+        break;
+      case('$merge'):
+        object = Object.assign(object, change);
+        break;
+      case('$set'):
+      console.log(object)
+        object = change;
+        newState = change;
+        break;
+      case('$apply'):
+        newState = change(state)
+        break;
+      default:
+        break;   
+    }
+  } 
   
   return newState;
 };
+module.exports.update = update;
