21c15e9314a56f114709b8d84dccc56a49d77085
mje1281 <m.ernst@rushordertees.com>
2017-03-06 19:21:47 -0500

    add function to process each promise and give a result to be used in resulting function

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,10 +1,22 @@
 function throttlePromises(limit, promiseArray){
   var promisesToDo = [];
   var i = 0;
+  
+  function nextAction() {
+    if (i < promiseArray.length) {
+      var actionIndex = i++;
+      var follower = promiseArray[actionIndex];
+      return Promise.resolve(follower())
+          .then(result => {  
+             resultArray[actionIndex] = result;
+             return;
+          }).then(nextAction);
+    }
+  }
+  
   while (i < limit && i < promiseArray.length) {
     new Promise.resolve(promiseArray[i]()).then(result){
       promisesToDo.push(result);
-    }
-    
+    }  
   }
 }
